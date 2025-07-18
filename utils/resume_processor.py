8e1b2ff2882d0e8a405b1dd538ca2031c728ec6a
nithul2005 <2005.nithulmadhu.a@gmail.com>
2025-07-18 10:16:50 +0530

    Add files via upload

---
+++ utils/resume_processor.py
@@ -0,0 +1,9 @@
+# utils/resume_processor.py
+
+class ResumeProcessor:
+    def __init__(self, resume_text):
+        self.resume_text = resume_text
+
+    def extract_keywords(self):
+        # Dummy keywords for now, can improve later
+        return ["Python", "Machine Learning", "AI"]
