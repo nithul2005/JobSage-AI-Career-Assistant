64d3bea5c3b3a25ee5a127abc840cadbb66763d8
nithul2005 <2005.nithulmadhu.a@gmail.com>
2025-07-18 12:05:11 +0530

    Delete utils

--- utils/resume_processor.py
+++
@@ -1,9 +0,0 @@
-# utils/resume_processor.py
-
-class ResumeProcessor:
-    def __init__(self, resume_text):
-        self.resume_text = resume_text
-
-    def extract_keywords(self):
-        # Dummy keywords for now, can improve later
-        return ["Python", "Machine Learning", "AI"]
